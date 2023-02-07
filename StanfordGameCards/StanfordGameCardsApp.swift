8169274267f4ebcbf807d31ca5ab87302abaf380
Nadia <ivashchenko.nadia@gmail.com>
2023-02-07 16:03:20 +0200

    Initial Commit

---
+++ StanfordGameCards/StanfordGameCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StanfordGameCardsApp.swift
+//  StanfordGameCards
+//
+//  Created by Students on 07.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct StanfordGameCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
